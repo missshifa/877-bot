d309f2a1ff7774b3236b0310cf49b11d87ff2deb
missshifa <nilaafrin733@gmail.com>
2025-09-04 10:37:44 +0600

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -1,35 +1,32 @@
 const fs = global.nodemodule["fs-extra"];
-‎module.exports.config = {
-‎  name: "goibot",
-‎  version: "1.0.1",
-‎  hasPermssion: 0,
-‎  credits: "Fixed By Arun Kumar",
-‎  description: "goibot",
-‎  commandCategory: "Noprefix",
-‎  usages: "noprefix",
-‎  cooldowns: 5,
-‎};
-‎module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
-‎  var { threadID, messageID, reason } = event;
-‎  const moment = require("moment-timezone");
-‎  const time = moment.tz("Asia/Kolkata").format("DD/MM/YYYY || HH:mm:ss");
-‎  var idgr = `${event.threadID}`;
-‎  var id = event.senderID;
-‎  var name = await Users.getNameUser(event.senderID);
-‎
-‎  var tl = ["পেট এ চাপ দিয়েই ছাতা বন্ধ করা লাগে.....!!","দেখ বিল্লু,,এইটা আমার বড় বিলাই🙄" , "বাবাগো......." , "কেন্দে দিয়েছি....🙈" , "বাড়িতে ফিরেই সবার প্রথম প্রশ্ন...মা কোথায়?💝" , "সাপুট দিলে সাপুট পাবেন....🫶" , "মেয়ে খাওয়ার মত ওয়েদার,,,,,🤤" ,  "আর যাবনা বেগুন তুলিতে,,,,,ও ললিতে.....🫵" , "বৃষ্টির দিনে রিকশা ভাড়া শুনলে মনে,, হয় বিয়ের গেট ধরছে.....☠️☠️" , "মহিলা কামড়ানো হয়....☹️" , "দোয়া অসম্ভব কে সম্ভব করতে পারে....।।" , "Bewafa Nikali re tu🙂🤨", "Systemmmmmmm 😴" , "নারী কত সুন্দর অভিনয় করে রে....🙈🙈", "পুরুষ কত সুন্দর অভিনয় করে রে....🙈🙈" , "Moye moye moye moye🙆🏻‍♀🙆🏻‍♀" , "Ye dukh kahe khatm nahi hota 🙁" , "Tum to dokebaz ho" , "you just looking like a wow😶" , "Babu, babu ammu basay nei" , "This person is unavailable on Messenger." ,"Jab dekho B0T B0T B0T😒😒", "জীবনটা শেখ হাসিনার মতো হয়ে গেছে,,,, যত কিছুই করি নাম নাই", "rag kore na pookie" , "Tum wahi ho na ,jisko.me.nahi janti 🙂" , "Ye I love you kya hota hai" , "Sunai deta hai mujhe behri nahi hu me   😒" , "so elegent, so beautiful , just looking like a wow🤭" , "valo ekta dalal suggest koren to 🙂" ,
-‎
-‎ "🤔" , "I Love you baby , mera recharge khtm hone wala h" , "জমি কিনতে আগ্রহী...😐" , "তুমি যদি নিজেকে ঘুষি মেরে ব্যথা পাও,,,, তাহলে কি তুমি শক্তিশালী নাকি দুর্বল....??" , "Arry Bas Kar🤣😛" , "Me ni To Kon Be" , "naam adiya kumar 7vi kaksha me padhte hai favret subject begon😘" , "Mera Dimag Mat Khaya kro😒😒" , "Chuppp Saatvi Fail😒" , "Saste Nashe Kab Band kroge" , "Mai Jaanu Ke sath Busy hu yar, mujhe mat balao" , "Haye Jaanu Mujhe Yaad Kiya🙈" , "Hayee ese mt bulaya kro, mujhe sharm aati h" , "System pe system betha rahi chhori bot ki" , "Naach meri Bulbul tujhe pesa milega" , "me idhar se hu aap kidhar se ho" , "Khelega Free Fire🙈🙈" , "aye haye oye hoye aye haye oye hoye😍 bado badi bado badi😘" , "e halo bhai darr rha hai kya" , "akh ladi bado badi" , "haaye garmi😕" , "Ao kabhi haweli pe😍" , "Khelega Free Fire🥴" , "Hallo bai tu darr raha hai kya" , "janu bula raha h mujhe" , "I cant live without you babu😘" , "haa meri jaan" , "Agye Phirse Bot Bot Krne🙄" , "konse color ki jacket pehne ho umm btao na😚" , "dhann khachh booyaah"];
-‎  var rand = tl[Math.floor(Math.random() * tl.length)]
-‎   mess = "{name}"
-‎  if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
-‎    var msg = {
-‎      body: `${rand}`
-‎    }
-‎    return api.sendMessage(msg, threadID, messageID);
-‎  };
-‎
-‎}
-‎
-‎module.exports.run = function({ api, event, client, __GLOBAL }) { }
-‎
+module.exports.config = {
+  name: "goibot",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Fixed By Arun Kumar",
+  description: "goibot",
+  commandCategory: "Noprefix",
+  usages: "noprefix",
+  cooldowns: 5,
+};
+module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
+  var { threadID, messageID, reason } = event;
+  const moment = require("moment-timezone");
+  const time = moment.tz("Asia/Kolkata").format("DD/MM/YYYY || HH:mm:ss");
+  var idgr = `${event.threadID}`;
+  var id = event.senderID;
+  var name = await Users.getNameUser(event.senderID);
+
+  var tl = ["Kya Tu ELvish Bhai Ke Aage Bolega🙄" , "Cameraman Jaldi Focus Kro 📸" , "Lagdi Lahore di aa🙈" , "Chay pe Chaloge" , "Mere liye Chay Bana Kar LA ,Pura din Dekho Bot BoT🙄" , "Din vicho tere Layi Teym Kadd ke, Kardi me Promise     Milan aungi" ,  "Yee bat Delhi tak jayegi" , "Je koi shaq ni , Kari check ni" , "ME HERAAN HU KI TUM BINA DIMAG KESE REH LETE HO☹️" , "sheHar me Hai rukka baeje Rao Saab ka🙄" , "Bewafa Nikali re tu🙂🤨", "Systemmmmmmm😴" , "Leja Leja tenu 7 samundra paar🙈🙈", "Laado puche manne kyu tera rang kala" , "Moye moye moye moye🙆🏻‍♀🙆🏻‍♀" , "Ye dukh kahe nahi khatm hota 🙁" , "Tum to dokebaz ho" , "you just looking like a wow😶" , "Mera aasmaan dhunde teri zamin" , "Kal ana abhi lunch time hai" , "Jab dekho B0T B0T b0T😒😒", "Chhodo na koi dekh lega🤭", "Kab ayega mere banjaare" , "Tum wahi ho na ,jisko.me.nahi janti 🙂" , "Ye I love you kya hota hai" , "Sunai deta hai mujhe behri nahi hu me   😒" , "so elegent, so beautiful , just looking like a wow🤭" , "began🙂" , "Aayein🤔" , "I Love you baby , mera recharge khtm hone wala h" , "paani paani uncle ji" , "apne Labhar ko dhoka do , daling hme bhi moka do🙈" , "Arry Bas Kar🤣😛" , "Me ni To Kon Be" , "naam adiya kumar 7vi kaksha me padhte hai favret subject begon😘" , "Mera Dimag Mat Khaya kro😒😒" , "Chuppp Saatvi Fail😒" , "Saste Nashe Kab Band kroge" , "Mai Jaanu Ke sath Busy hu yar, mujhe mat balao" , "Haye Jaanu Mujhe Yaad Kiya🙈" , "Hayee ese mt bulaya kro, mujhe sharm aati h" , "System pe system betha rahi chhori bot ki" , "Naach meri Bulbul tujhe pesa milega" , "me idhar se hu aap kidhar se ho" , "Khelega Free Fire🙈🙈" , "aye haye oye hoye aye haye oye hoye😍 bado badi bado badi😘" , "e halo bhai darr rha hai kya" , "akh ladi bado badi" , "haaye garmi😕" , "Ao kabhi haweli pe😍" , "Khelega Free Fire🥴" , "Hallo bai tu darr raha hai kya" , "janu bula raha h mujhe" , "I cant live without you babu😘" , "haa meri jaan" , "Agye Phirse Bot Bot Krne🙄" , "konse color ki jacket pehne ho umm btao na😚" , "dhann khachh booyaah"];
+  var rand = tl[Math.floor(Math.random() * tl.length)]
+   mess = "{name}"
+  if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
+    var msg = {
+      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : ཫ༄𒁍≛⃝SHIFAT `
+    }
+    return api.sendMessage(msg, threadID, messageID);
+  };
+
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) { }
